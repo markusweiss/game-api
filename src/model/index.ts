2c9b67c0c7fa82fcc060fdbd1f3edf32e0e8277c
Markus Weiss <markus.weiss@unterschiedundmacher.com>
2022-03-24 07:42:20 +0100

    test formatting

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,32 +2,32 @@ import { DataTypes, Model } from 'sequelize';
 import db from '../config/database.config';
 
 interface DataAttributes {
-    id: string;
-    title: string;
-    completed: boolean;
+	id: string;
+	title: string;
+	completed: boolean;
 }
 
 export class DataInstance extends Model<DataAttributes> {}
 
 DataInstance.init(
-    {
-        id: {
-            type: DataTypes.UUIDV4,
-            primaryKey: true,
-            allowNull: false
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        completed: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false
-        }
-    },
-    {
-        sequelize: db,
-        tableName: 'TestTable'
-    }
+	{
+		id: {
+			type: DataTypes.UUIDV4,
+			primaryKey: true,
+			allowNull: false
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		completed: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	},
+	{
+		sequelize: db,
+		tableName: 'TestTable'
+	}
 );
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import db from './config/database.config';
 import { DataInstance } from './model';
 
 db.sync().then(() => {
-    console.log('DB connect ok');
+	console.log('DB connect ok');
 });
 
 const app = express();
@@ -14,20 +14,20 @@ const port = '9000';
 app.use(express.json());
 
 app.post('/create', async (req: Request, res: Response) => {
-    const id = uuidv4();
-    try {
-        console.log(req.body);
-        const record = await DataInstance.create({ ...req.body, id });
-        return res.json({ record, msg: 'created' });
-    } catch (e) {
-        return res.json({
-            msg: 'fail to create',
-            status: 500,
-            route: '/create'
-        });
-    }
+	const id = uuidv4();
+	try {
+		console.log(req.body);
+		const record = await DataInstance.create({ ...req.body, id });
+		return res.json({ record, msg: 'created' });
+	} catch (e) {
+		return res.json({
+			msg: 'fail to create',
+			status: 500,
+			route: '/create'
+		});
+	}
 });
 
 app.listen(port, () => {
-    console.log('Server on Port: ' + port);
+	console.log('Server on Port: ' + port);
 });
